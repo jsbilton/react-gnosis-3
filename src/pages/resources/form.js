c627b06ce7704c30228a7b36f0eaeaa4e6792b4f
Jared Bilton <jsbilton@gmail.com>
2016-11-17 23:43:11 -0500

    initial commit

---
+++ src/pages/resources/form.js
@@ -0,0 +1,74 @@
+const React = require('react')
+
+const dbUrl = process.env.REACT_APP_DB
+const PouchDB = require('pouchdb-http')
+const db = PouchDB(dbUrl)
+
+
+const ResourceForm = React.createClass({
+    getInitialState() {
+        return {
+          error:'',
+          result: {},
+            resource: {
+                title: "My name",
+                reference: "My stuff",
+                // _id: new Date().toISOString()
+            }
+        }
+    },
+    handleChange(field) {
+        return e => {
+            let resource = this.state.resource
+            resource[field] = e.target.value
+            this.setState({resource})
+        }
+    },
+    handleSubmit(e) {
+        e.preventDefault()
+        const resource = this.state.resource
+        resource._id = new Date().toISOString()
+        db.put(this.state.resource, (err, result) => {
+          if (err) return this.setState({ error: err.message })
+          this.setState({ result: result })
+        })
+    },
+    render() {
+
+      const showError = _ =>
+        this.state.error !== '' ?
+          <div>{this.state.error}</div>
+          : null
+      const showResult = _ =>
+        this.state.error !== '' ?
+        <div>{this.state.result}</div>
+          : null
+
+        return (
+            <div>
+              { showError() }
+              { showResult() }
+                <form onSubmit={this.handleSubmit}>
+                    <div>
+                        <label>Title</label>
+                        <input onChange={this.handleChange('title')} value={this.state.resource.title}/>
+                    </div>
+                    <div>
+                        <label>Reference</label>
+                        <input onChange={this.handleChange('reference')} value={this.state.resource.reference}/>
+                    </div>
+                    <div>
+                        <button>Add to Database</button>
+                    </div>
+                </form>
+                <hr/>
+                <pre>
+                  {JSON.stringify(this.state.resource)}
+                  {JSON.stringify(this.state.error)}
+                </pre>
+            </div>
+        )
+    }
+})
+
+module.exports = ResourceForm
---
+++ src/pages/resources/index.js
@@ -0,0 +1,44 @@
+const React = require('react')
+const R = require('ramda')
+
+const dbUrl = process.env.REACT_APP_DB
+const PouchDB = require('pouchdb-http')
+const db = PouchDB(dbUrl)
+
+const Resources = React.createClass({
+    getInitialState() {
+        return {resources: []}
+    },
+    componentDidMount() {
+        db.allDocs({
+            include_docs: true
+        }, (e, r) => {
+            if (e)
+                return this.setState({error: e.message})
+            this.setState({
+                resources: R.pluck('doc', r.rows)
+            })
+        })
+    },
+    render() {
+        const listResource = doc =>
+        <li key={doc._id}>
+            <a href={doc.reference}>
+              {doc.title}
+            </a>
+        </li>
+
+        return (
+            <div>
+                <h3>Resources</h3>
+                <ul>
+                    {this.state.resources.map(listResource)}
+                </ul>
+            </div>
+        )
+    }
+})
+
+
+
+module.exports = Resources
